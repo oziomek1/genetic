f301758f9ffdd3ec4299ca1cf4c80717e08ad75d
oziomek1 <ozimekwojciech@zoho.eu>
2018-01-08 01:29:02 +0100

    Created Candidate and Population, need to upgrade Candidate->getFitness() method

---
+++ Candidate/Candidate.hpp
@@ -0,0 +1,73 @@
+//
+// Created by wojtek on 2018-01-07.
+//
+
+#ifndef GENETIC_CANDIDATE_HPP
+#define GENETIC_CANDIDATE_HPP
+#include <array>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include "../Algorithm/FitnessCalc.hpp"
+
+class Candidate {
+    const int id;
+    int fitness = 0; // works as a cache
+    static int genesLength = 50;
+    friend std::ostream & operator<<(std::ostream &os, const Candidate& candidate);
+public:
+    std::array<bool, genesLength> genesArray{};
+    void generateGenes() {
+        for(int i = 0; i < genesArray.size(); i++) {
+            genesArray[i] = (rand() % 2 == 0);
+        }
+    }
+
+    static void setGenesLength(int &length) {
+        genesLength = length;
+    }
+
+    bool getGeneAtIndex(int &index) {
+        return genesArray[index];
+    }
+
+    void setGeneAtIndex(int &index, const bool &value) {
+        genesArray[index] = value;
+        fitness = 0;
+    }
+
+    int size() {
+        return genesArray.size();
+    }
+
+    int getId() {
+        return id;
+    }
+
+    int getFitness() {
+        if (fitness == 0) {
+            FitnessCalc fitnessCalc;
+            return fitnessCalc.getFitness(nullptr);
+        }
+        return fitness;
+    }
+
+    std::string const& to_string() const {
+        std::stringstream gene_stream;
+        gene_stream << "Gene " << id << ": ";
+        std::string gene_data;
+        for(int i = 0; i < genesArray.size(); i++) {
+            gene_data += genesArray[i];
+        }
+        std::string gene_string = gene_stream.str();
+        gene_string += gene_data;
+        return gene_string;
+    }
+};
+
+std::ostream & operator<<(std::ostream &os, const Candidate& candidate) {
+    return os << candidate.to_string();
+}
+
+#endif //GENETIC_CANDIDATE_HPP
---
+++ Population/Population.hpp
@@ -0,0 +1,66 @@
+//
+// Created by wojtek on 2018-01-07.
+//
+
+#ifndef GENETIC_POPULATION_HPP
+#define GENETIC_POPULATION_HPP
+
+#include "../Candidate/Candidate.hpp"
+#include <vector>
+#include <iterator>
+class Population {
+    std::vector<Candidate*> candidates{};
+    int populationSize;
+public:
+    Population(int populationSize_, bool init) : populationSize(populationSize_) {
+        if(init) {
+            for (int i = 0; i < populationSize; i++) {
+                Candidate *newCandidate = new Candidate();
+                newCandidate->generateGenes();
+                candidates.push_back(newCandidate);
+            }
+        }
+    }
+
+    Candidate *getCandidate(int index) {
+        return candidates[index];
+    }
+
+    Candidate *getCandidate(std::vector<Candidate*>::iterator it) {
+        return *it;
+    }
+
+//    Candidate getFittest() {
+//        Candidate *fittest = candidates.front();
+//
+//        for(std::vector<Candidate*>::iterator it = candidates.begin(); it != candidates.end(); ++it) {
+//            if(fittest->getFitness() <= getCandidate(it)->getFitness()) {
+//                fittest = getCandidate(it);
+//            }
+//        }
+//        return *fittest;
+//    }
+
+    Candidate getFittest() {
+        Candidate *fittest = candidates.front();
+
+        for(int i = 0; i < size(); i++) {
+            if(fittest->getFitness() <= getCandidate(i)->getFitness()) {
+                fittest = getCandidate(i);
+            }
+        }
+        return *fittest;
+    }
+
+    int size() {
+        return candidates.size();
+    }
+
+    void saveCandidateAtIndex(int index, Candidate *candidate) {
+        candidates[index] = candidate;
+    }
+
+};
+
+
+#endif //GENETIC_POPULATION_HPP
